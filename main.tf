60a05dd0913796199ec7509bb04ee076b455daea
krishnavamshi933 <109383860+krishnavamshi933@users.noreply.github.com>
2023-07-05 01:53:13 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -39,7 +39,7 @@ resource "aws_security_group" "nginx_sg" {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    security_groups = [aws_security_group.django_sg.id]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
@@ -63,6 +63,28 @@ resource "aws_security_group" "django_sg" {
     protocol        = "tcp"
     security_groups = [aws_security_group.nginx_sg.id]
   }
+
+  ingress {
+    from_port       = 22
+    to_port         = 22
+    protocol        = "tcp"
+    cidr_blocks     = var.allowed_ssh_cidr_blocks
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# Create DB Server Security Group
+resource "aws_security_group" "db_sg" {
+  name        = "db-sg"
+  description = "Security group for PostgreSQL database server"
+
+  vpc_id = var.vpc_id
 
   ingress {
     from_port       = 22
@@ -132,6 +154,24 @@ resource "aws_instance" "django_app_2" {
   }
 }
 
+# Create PostgreSQL Database Server
+resource "aws_instance" "postgres_db" {
+  ami           = "ami-0123456789"  # Replace with your desired PostgreSQL AMI ID
+  instance_type = "t2.micro"  # Replace with your desired instance type
+  key_name      = var.key_pair_name
+  associate_public_ip_address = false  # Disable public IP for the PostgreSQL server
+
+  tags = {
+    Name = "postgres-db"
+  }
+
+  network_interface {
+    device_index    = 0
+    subnet_id       = var.subnet_id
+    security_groups = [aws_security_group.db_sg.id]
+  }
+}
+
 # Output
 output "nginx_lb_public_ip" {
   value = aws_instance.nginx_lb.public_ip
@@ -143,4 +183,8 @@ output "django_app_1_private_ip" {
 
 output "django_app_2_private_ip" {
   value = aws_instance.django_app_2.private_ip
+}
+
+output "postgres_db_private_ip" {
+  value = aws_instance.postgres_db.private_ip
 }
